79ca513834d04989062a8acf766ed0a80b1a333b
cchura94 <cchura.cpc@gmail.com>
2022-04-13 00:12:06 -0400

    Listado de Usuarios

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // select * from users;
+        $users = User::paginate(1);
+        return response()->json($users, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,7 +19,12 @@ Route::group(["prefix" => "/v1/auth"], function(){
     Route::group(["middleware" => "auth:sanctum"], function(){
         Route::get("/perfil", [AuthController::class, "perfil"]);
         Route::post("/logout", [AuthController::class, "salir"]);
-    });    
+    });  
 });
+
+Route::group(["middleware" => "auth:sanctum"], function(){
+    // rutas seguras
+    Route::apiResource("/usuario", UsuarioController::class);
+}); 
 
 // nuevo cambio
